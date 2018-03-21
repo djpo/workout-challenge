4e856fed0b2a9e7039210393b0604956e5500c1f
djpo <djpo@users.noreply.github.com>
2018-03-21 16:41:37 +0100

    move redux initialState to separate file

---
+++ app/redux/initialState.js
@@ -0,0 +1,66 @@
+const initialState = {
+  workouts: [
+    {
+      date: '20180321',
+      participants: [
+        {
+          id: '001',
+          name: "Drk Prp",
+          checkedIn: false,
+        },
+        {
+          id: '002',
+          name: "Aab Ccd",
+          checkedIn: false,
+        },
+        {
+          id: '003',
+          name: "Eef Ggh",
+          checkedIn: true,
+        },
+      ],
+    },
+    {
+      date: '20180322',
+      participants: [
+        {
+          id: '002',
+          name: "Aab Ccd",
+          checkedIn: false,
+        },
+        {
+          id: '003',
+          name: "Eef Ggh",
+          checkedIn: false,
+        },
+        {
+          id: '004',
+          name: "Iij Kkl",
+          checkedIn: false,
+        },
+      ],
+    },
+    {
+      date: '20180323',
+      participants: [
+        {
+          id: '004',
+          name: "Iij Kkl",
+          checkedIn: true,
+        },
+        {
+          id: '005',
+          name: "Mmn Oop",
+          checkedIn: false,
+        },
+      ],
+    },
+    {
+      date: '20180326',
+      participants: [],
+    },
+  ],
+  selectedWorkout: '',
+};
+
+export default initialState;
